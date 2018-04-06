4031a2a8dd6268672aeb24dfa38469b1d86f2313
enuggetry <ericiam@berkeley.edu>
2018-04-06 11:44:58 -0700

    add scripts

---
+++ postinstall.js
@@ -0,0 +1,14 @@
+const approot = require('app-root-path');
+const sh = require('shelljs');
+const fs = require('fs-extra');
+
+let target = approot+'/blastdb';
+let src = 'algae.tar.gz';
+
+fs.ensureDirSync(target);
+
+sh.exec('cp '+src+' '+target);
+
+process.chdir(target);
+
+sh.exec('tar -zxvf algae.tar.gz');
