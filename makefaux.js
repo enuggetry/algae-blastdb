c249f3798553efa809715f8f465db6a81b1f4179
enuggetry <github@meyao.net>
2018-04-17 20:59:51 -0700

    add capability to generate a lot more faux organisms.

--- makefaux.js
+++ makefaux.js
@@ -1,55 +1,114 @@
-const Fasta = require('biojs-io-fasta');
 const _ = require('lodash');
 const fs = require('fs-extra');
+const fetch = require('node-fetch');
+const async = require('async');
+//var rand = require('random-seed').create("faux");
 
 let url = 'file:///var/www/html/blasttest/volvox.fa';
 let inFile = 'volvox.fa';
 let outFile = 'fauxdb.fa';
-let count = 0;
-let seq = "";
-let mi = .05;	// mutations per iteration.
+let mi = .04;	// mutations per iteration.
 let lineLen = 70;
+let organismCount = 0;
+let organismSeries = [10,12,15,20,25,30,40,50];
 
-// create new file
-fs.writeFileSync(outFile,"");
+let animalsUrl = 'https://raw.githubusercontent.com/boennemann/animals/master/words.json';
+let ajectivesUrl = 'https://raw.githubusercontent.com/dariusk/corpora/master/data/words/adjs.json';
+let organismList = {};  // list of used organisms
+let nameDb = null;
 
-let fauxOrganisms = [
-	"flaccidos caede bimembres certatimque",	// flaccid centaur
-	"quisquiliae minotuar",						// trashy minotuar
-	"draco hyacintho gelata",					// blue jelly dragon
-	"puer aureus homine sene",					// orange-haired man-child
-	"deturpant simia",							// grease monkey
-	"kessinger avis malae texere",				// fanged dodo bird
-	"tigris de niliacis",						// hooting tiger
-	"cyclopis ebrius est"						// drunk cyclops
-];
+//fs.writeFileSync(outFile,"");   // create new fa file
+let writeStream = fs.createWriteStream(outFile);
 
+// read animals and ajectives
+async.parallel({
+    animals: function(cb) {
+        fetch(animalsUrl)
+            .then(res => res.json())
+            .then(json => {
+                cb(null, json);
+            }
+        );
+    },
+    adjectives: function(cb) {
+        fetch(ajectivesUrl)
+            .then(res => res.json())
+            .then(json => {
+                cb(null, json.adjs);
+            }
+        );
+    }
+}, function(err, results) {
+    // done reading animals and ajectives
+    if (err) return;
+    //console.log('results',results);
+    nameDb = results;
 
-lineReader = require('readline').createInterface({
-  input: require('fs').createReadStream(inFile)
+    generate();
+    
 });
 
-lineReader.on('line', function (line) {
-  //console.log(line);
-  if (line[0] !== '>') {
-	count++;
-	seq += line;
-  }
-});
-lineReader.on('close', function () {
-	seq = seq.toLowerCase(seq);
-	let origSeq = _.clone(seq);
-	
-	for (let i=0;i<8;i++) {
-		seq = mutate(seq,mi);
-		exportFauxSeq(seq,fauxOrganisms[i],outFile);
-		console.log("% change " + verifyPercentChange(origSeq,seq));
-	}
-	seq = mutate(seq,mi);
-	//console.log("mutated seq",seq);
-	console.log("done "+seq.length+" characters");
-	//console.log("% change " + verifyPercentChange(origSeq,seq));
-});
+function generateOrganismName() {
+    while(true) {
+        let animal = nameDb.animals[Math.floor(Math.random() * nameDb.animals.length)];
+        let adjective = nameDb.adjectives[Math.floor(Math.random() * nameDb.adjectives.length)];
+        let newName = adjective+' '+animal;
+        
+        // ensure no two organism names are the same.
+        if (typeof organismList[newName] === 'undefined') {
+            organismList[newName] = true;
+            return newName;
+        }   
+    }
+}
+
+function generate() {
+    // assuming openFiles is an array of file names
+    async.eachSeries(organismSeries, function(n, cb) {
+        generateMutants(n,function() {
+            cb();
+        });
+    }, function(err) {
+        if( err ) {
+          console.log('An error occurred');
+        } else {
+          console.log('done - organismCount',organismCount);
+        }
+    });    
+}
+
+// generate n muntants, each mutant is 5% different than the previous
+function generateMutants(n,done) {
+    let seq = "";
+    lineReader = require('readline').createInterface({
+      input: fs.createReadStream(inFile)
+    });
+
+    lineReader.on('line', function (line) {
+      //console.log(line);
+      if (line[0] !== '>') {
+            //count++;
+            seq += line;
+      }
+    });
+    lineReader.on('close', function () {
+            seq = seq.toLowerCase(seq);
+            let origSeq = _.clone(seq);
+
+            for (let i=0;i<n;i++) {
+                    seq = mutate(seq,mi);
+                    let name = generateOrganismName();
+                    exportFauxSeq(seq,name,outFile);
+                    console.log("% change " + verifyPercentChange(origSeq,seq));
+                    organismCount++;
+            }
+            seq = mutate(seq,mi);
+            //console.log("mutated seq",seq);
+            console.log("done "+seq.length+" characters");
+            //console.log("% change " + verifyPercentChange(origSeq,seq));
+            done();
+    });
+}
 
 function mutate(seq,pct) {
 	let mCount = Math.floor(seq.length * pct);
@@ -103,9 +162,10 @@ function precisionRound(number, precision) {
 
 function exportFauxSeq(seq,header,file) {
 	let count = Math.floor(seq.length / lineLen);
-	fs.appendFileSync(file,"\n>"+header+"\n");
+	//fs.appendFileSync(file,"\n>"+header+"\n");
+        writeStream.write("\n>"+header+"\n")
 	for(let i=0;i<count+1;i++) {
 		//console.log(seq.substr(i*lineLen,lineLen));
-		fs.appendFileSync(file,seq.substr(i*lineLen,lineLen)+"\n");
+		writeStream.write(seq.substr(i*lineLen,lineLen)+"\n");
 	}
 }
